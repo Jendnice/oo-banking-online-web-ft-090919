50e0c466f4833a531f0a7fc378701eb45d6503ee
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-25 18:06:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end 
 
   def execute_transaction
-    if @sender.valid? == false  
+    if sender.valid? == false  
        @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     elsif 
