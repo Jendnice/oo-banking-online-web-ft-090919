dd1992147d9c2d4a74023cfdcf91a06315b9c916
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-25 17:52:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       @sender.balance -= @amount 
       @receiver.balance += @amount
       @status = "complete"
-    else @sender.close_account == true   
+    else    
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end 
