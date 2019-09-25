20e1af8e223649714f7e99fb67947956cae95960
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-25 17:24:46 +0000

    Automatically backed up by Learn

--- notes.rb
+++ notes.rb
@@ -15,17 +15,12 @@ class Transfer
 
   def execute_transaction
     if @sender.balance < @amount || @status == "closed"
-      
-      
-      
-      
-    if @sender.balance > @amount && @status == "pending"
+       @status = "rejected"
+       return "Transaction rejected. Please check your account balance."
+    else @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
     end
   end
 
