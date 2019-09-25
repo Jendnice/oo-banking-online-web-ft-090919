a8d382cb3913f7416064b51e4ac5f8a0c2f79780
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-25 17:48:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,18 +25,6 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end 
   end 
-  
-  # def execute_transaction
-  #   if @sender.close_account == true 
-  #     @status = "rejected"
-  #     return "Transaction rejected. Please check your account balance."
-  #   elsif !(@sender.close_account) == true && @sender.balance > @amount && @status == "pending"
-  #     @sender.balance -= @amount 
-  #     @receiver.balance += @amount
-  #     @status = "complete"
-  #   end 
-  # end 
-  
    
  
 end
