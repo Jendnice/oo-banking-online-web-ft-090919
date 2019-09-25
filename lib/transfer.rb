d6b3a823c650fc6e133802a0514b1949f1e0750a
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-25 18:14:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,6 +16,7 @@ class Transfer
   end 
 
   def execute_transaction
+    binding.pry
     if sender.valid? == false  
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
