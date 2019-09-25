cd8ada3163bf867626c97ffb22ef0c6968bc8300
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-25 17:38:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,23 +15,25 @@ class Transfer
     @sender.valid? && @receiver.valid? ? true : false 
   end 
 
-  def execute_transaction
-    if @sender.balance > @amount && @status == "pending" 
-      @sender.balance -= @amount 
-      @receiver.balance += @amount
-      @status = "complete"
-    else close_account = true   
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end 
-  end 
+  # def execute_transaction
+  #   if @sender.balance > @amount && @status == "pending" 
+  #     @sender.balance -= @amount 
+  #     @receiver.balance += @amount
+  #     @status = "complete"
+  #   else close_account = true   
+  #     @status = "rejected"
+  #     return "Transaction rejected. Please check your account balance."
+  #   end 
+  # end 
   
   def execute_transaction
     if close_account = true 
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     elsif close_account = false && sender.balance > @amount && @status == "pending"
-      sender.balance
+      @sender.balance -= @amount 
+      @receiver.balance += @amount
+      @status = "complete"
     
       
   
